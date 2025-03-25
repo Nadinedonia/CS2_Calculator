fcf376f2fa99e65498549edaa860c7aa86abef53
Ahmedelshabrawi <ahmedelshabrawi@aucegypt.edu>
2025-03-25 14:11:34 +0200

    Files

--- CS2-Lab-HW/caclulator.h
+++ CS2-Lab-HW/caclulator.h
@@ -5,3 +5,4 @@ double division(double num1 ,double num2);
 int factorial (int num);
 int gcd(int num1,int num2);
 int lcm (int num1,int num2)
+int random(int num1, int num2);
--- CS2-Lab-HW/calculator.cpp
+++ CS2-Lab-HW/calculator.cpp
@@ -1,13 +1,39 @@
-
-
-
-
-
-
+double addition(double num1, double num2);
+{
+    return num1+num2;
+}
+double subtraction(double num1,double num2);
+{
+    return num1-num2;
+}
+double multiplication(double num1, double num2 );
+{
+    return num1*num2;
+}
+double division(double num1 ,double num2);
+{
+    return num1/num2;
+}
+int factorial (int num);
+{
+    return num*factorial(num-1);
+}
 int gcd(int num1, int num2 )
 {
     if (num2 !=0)
         return gcd(num1,num1 %/num2 );
     else
         return num1;
+}
+int random(int max, int min )
+{
+    int r;
+    int num;
+    num=max-min+1
+    r=min+ rand()%num;
+    return r;
+}
+int lcm(int x, int y)
+{
+        return (x / gcd(x, y)) * y;
 }
